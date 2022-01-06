87b1777312c3718998ad20bdc8c6a49ad524669e
SuchetaJ <72176192+SuchetaJ@users.noreply.github.com>
2022-01-06 09:19:15 +0530

    Delete practice1.sql

--- RDBMS/practice1.sql
+++
@@ -1,9 +0,0 @@
-create database practice1;
-show databases;
-use practice1;
-create table students(rull_num int, f_name varchar(20), grade varchar(2), result varchar(4));
-show tables;
-insert into students values('01','Harry Potter','AB','pass');
-insert into students values(002,'Ginny Weasley','A','pass');
-insert into students values(3,'Fred Weasley','F','fail');
-select * from students;
